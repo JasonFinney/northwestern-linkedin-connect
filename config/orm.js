f28dbeae37d8412d092d29ea77750b81e669954b
Jason Finney <finney.jason.218@gmail.com>
2018-09-15 00:47:23 -0500

    we are on the testing phase

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,3 @@
-// Import MySQL connection.
 var connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
@@ -39,7 +38,6 @@ function objToSql(ob) {
     return arr.toString();
 }
 
-// Object for all our SQL statement functions.
 var orm = {
     all: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -50,19 +48,21 @@ var orm = {
             cb(result);
         });
     },
-    create: function (table, cols, vals, cb) {
+    create: function (table, cols, val1, val2, cb) {
         var queryString = "INSERT INTO " + table;
 
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
+        queryString += printQuestionMarks(val1.length);
+        queryString += ", ";
+        queryString += printQuestionMarks(val2.length);
         queryString += ") ";
 
         console.log(queryString);
 
-        connection.query(queryString, vals, function (err, result) {
+        connection.query(queryString, [{ val1 }, { val2 }], function (err, result) {
             if (err) {
                 throw err;
             }
@@ -70,7 +70,6 @@ var orm = {
             cb(result);
         });
     },
-    // An example of objColVals would be {name: panther, sleepy: true}
     update: function (table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
 
@@ -88,20 +87,6 @@ var orm = {
             cb(result);
         });
     },
-    delete: function (table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
-        queryString += " WHERE ";
-        queryString += condition;
-
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
-    }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
--- controllers/linkedinController.js
+++ controllers/linkedinController.js
@@ -1,42 +1,36 @@
 var express = require("express");
-
 var router = express.Router();
+var linkedin = require("../models/linkedin");
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
-
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    cat.all(function (data) {
+    linkedin.all(function (data) {
         var hbsObject = {
-            cats: data
+            linkedin: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 
-router.post("/api/cats", function (req, res) {
-    cat.create([
-        "name", "sleepy"
+router.post("/api/linkedin", function (req, res) {
+    linkedin.create([
+        "name", "url", "entered"
     ], [
-            req.body.name, req.body.sleepy
+            req.body.name, req.body.url, req.body.entered
         ], function (result) {
-            // Send back the ID of the new quote
             res.json({ id: result.insertId });
         });
 });
 
-router.put("/api/cats/:id", function (req, res) {
+router.put("/api/linkedin/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
-    cat.update({
-        sleepy: req.body.sleepy
+    linkedin.update({
+        entered: req.body.entered
     }, condition, function (result) {
         if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -44,18 +38,4 @@ router.put("/api/cats/:id", function (req, res) {
     });
 });
 
-router.delete("/api/cats/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    cat.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-// Export routes for server.js to use.
 module.exports = router;
